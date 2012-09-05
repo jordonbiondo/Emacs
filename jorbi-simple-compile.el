84b62aa2d130ff8ce7ad638f1debb244fb3dfbd9
Jordon Biondo <biondoj@mail.gvsu.edu>
2012-09-05 15:18:15 -0400

    Compile file

---
+++ jorbi-simple-compile.el
@@ -0,0 +1,50 @@
+
+;;
+;; Simple, single file, compiles
+;;
+(defun jorbi-simple-compile()
+  "Compiles only the current buffer.\nSupported Types: Java, C"
+  (interactive)
+  (cond 
+   ((string= mode-name "C/l") (jorbi-simple-compile-c99))
+   ((string= mode-name "Java/l") (jorbi-simple-compile-java))
+   ((string= mode-name "Go") (jorbi-simple-compile-go))
+   (t (print "not implemented"))
+  )
+)
+
+
+;;
+;; Java single file compile
+;;
+(defun jorbi-simple-compile-java()
+  "Compile a single java buffer"
+  (interactive)
+  (compile (concat "javac " buffer-file-name))
+)
+
+;;
+;; Gcc c99 compile
+;;
+(defun jorbi-simple-compile-c99()
+  "Compile a single C buffer with -std=C99"
+  (interactive)
+  (compile (concat (concat (concat "gcc -std=c99 " buffer-file-name) " -o ")
+		   (car (split-string (car (last (split-string buffer-file-name "/"))) "\\.")))
+  )
+)
+
+;;
+;; Simple Go compile
+;;
+(defun jorbi-simple-compile-go()
+  "Compile a single Go Buffer"
+  (interactive)
+  (print "GO IS NOT ADDED TO THE PATH")
+  (compile (concat (concat "go build -o " 
+			   (jorbi-file-name)
+		   ) 
+		   (concat " " buffer-file-name)
+	   )
+  )
+)
--- jorbi.el
+++ jorbi.el
@@ -4,7 +4,6 @@
 ;;
 ;;----------------------------------------------
 
-
 ;;
 ;; Init
 ;;
@@ -91,58 +90,23 @@
 ;;
 ;; Comment macro
 ;;
+(defun jorbi-doc-comment()
+  (interactive)
+  (cond
+   ((string= mode-name "Emacs-Lisp") (jorbi-el-doc-comment-macro))
+   (t (jorbi-doc-comment-macro))
+  )
+)
+
+;;
+;; Macros
+;;
 (fset 'jorbi-doc-comment-macro
    [?\C-a return ?\C-p tab ?/ ?* return ?* return ?* ?/ ?\C-p ? ])
 
-;;
-;; Simple, single file, compiles
-;;
-(defun jorbi-simple-compile()
-  "Compiles only the current buffer.\nSupported Types: Java, C"
-  (interactive)
-  (cond 
-   ((string= mode-name "C/l") (jorbi-simple-compile-c99))
-   ((string= mode-name "Java/l") (jorbi-simple-compile-java))
-   ((string= mode-name "Go") (jorbi-simple-compile-go))
-   (t (print "not implemented"))
-  )
-)
+(fset 'jorbi-el-doc-comment-macro
+   [?\C-a return ?\C-p tab ?\; ?\; return ?\; ?\; return ?\; ?\; up ? ])
 
-
-;;
-;; Java single file compile
-;;
-(defun jorbi-simple-compile-java()
-  "Compile a single java buffer"
-  (interactive)
-  (compile (concat "javac " buffer-file-name))
-)
-
-;;
-;; Gcc c99 compile
-;;
-(defun jorbi-simple-compile-c99()
-  "Compile a single C buffer with -std=C99"
-  (interactive)
-  (compile (concat (concat (concat "gcc -std=c99 " buffer-file-name) " -o ")
-		   (car (split-string (car (last (split-string buffer-file-name "/"))) "\\.")))
-  )
-)
-
-;;
-;; Simple Go compile
-;;
-(defun jorbi-simple-compile-go()
-  "Compile a single Go Buffer"
-  (interactive)
-  (print "GO IS NOT ADDED TO THE PATH")
-  (compile (concat (concat "go build -o " 
-			   (jorbi-file-name)
-		   ) 
-		   (concat " " buffer-file-name)
-	   )
-  )
-)
 
 ;;
 ;; Extract filename
@@ -152,6 +116,7 @@
   (interactive)
   (car (split-string (car (last (split-string buffer-file-name "/"))) "\\."))
 )
+
 ;;
 ;; Switch to termainl buffer
 ;;
