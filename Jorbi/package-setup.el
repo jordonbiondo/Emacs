807b21d09fb64bec05ee29b9374f5de11269ddac
Jordon Biondo <biondoj@mail.gvsu.edu>
2013-04-14 20:44:06 -0400

    provide package setup

--- Jorbi/package-setup.el
+++ Jorbi/package-setup.el
@@ -1,49 +1,49 @@
-;;; package-setup.el --- 
-;; 
+;;; package-setup.el ---
+;;
 ;; Filename: package-setup.el
-;; Description: 
+;; Description:
 ;; Author: Jordon Biondo
-;; Maintainer: 
+;; Maintainer:
 ;; Created: Fri Apr 12 12:21:11 2013 (-0400)
-;; Version: 
+;; Version:
 ;; Last-Updated: Fri Apr 12 12:21:30 2013 (-0400)
 ;;           By: Jordon Biondo
 ;;     Update #: 2
-;; URL: 
-;; Doc URL: 
-;; Keywords: 
-;; Compatibility: 
-;; 
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 
@@ -56,6 +56,8 @@
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
+
+(provide 'package-setup)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; package-setup.el ends here
