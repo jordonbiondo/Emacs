96330fff56bc87e3922254716cabc4201b137529
Jordon Biondo <biondoj@mail.gvsu.edu>
2013-04-21 22:15:34 -0400

    can't set a var that doesnt' exist yet!

--- Jorbi/org-setup.el
+++ Jorbi/org-setup.el
@@ -17,37 +17,37 @@
 ;;; Commentary:
 ;;
 ;; Customization for `org-mode'
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 
 (require 'org-bullets)
 
-(setq org-bullets-bullet-list jorbi-org-bullets-bullet-list)
+
 
 (defcustom jorbi-org-bullets-bullet-list
   '(;;; Large
@@ -61,6 +61,9 @@
 It can contain any number of symbols, which will be repeated."
   :group 'org-bullets
   :type '(repeat (string :tag "Bullet character")))
+
+
+(setq org-bullets-bullet-list jorbi-org-bullets-bullet-list)
 
 
 (provide 'org-setup)
